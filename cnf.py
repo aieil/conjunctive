1f57b2506ffb34e481d925a89b2f0fa4d7c85720
Joseph Landy <14jnl@queensu.ca>
2017-02-15 21:32:50 -0500

    not thing eliminate ->

--- cnf.py
+++ cnf.py
@@ -1,35 +1,5 @@
 import logicparse as lp
 
-def group_precidence(formulaString):
-    formula = lp.nestgen(lp.parse(formulaString))
-    # now have to group by operator precidence.
-    # am I spelling precidence wrong?
-    # <-> binds least tightly.
-    # prof seemed to indicate in an example on slack that
-    # for A <-> B <-> C or whatever, to group it like
-    # (A<->B)<->C
-
-def elim_iff(formula):
-    iffIndices = findall(formula, '<->')
-    if iffIndices != []:
-        # the last one is at the top level since it bind least tightly.
-        lastIffIndex = iffIndices[-1]
-        termA = elim_iff(formula[:lastIffIndex]) # list up to end.
-        print(termA)
-        termB = elim_iff(formula[lastIffIndex + 1:])
-        # rest of list. Will not contain iff.
-        formula = [[termA, '->', termB], '^', [termB, '->', termA]]
-
-
-    # if neither of these is found, loop through bracketed expressions.
-    return formula
-    # otherwise: loop through any bracketed expressions.
-    # else:
-    #     for i in range(len(formula)):
-    #         if type(formula[i]) is list:
-    #             formula[i] = elim_iff(formula[i])
-    #
-    # return formula
 
 def elim(formula):
     # combined elimination of <-> and ->
@@ -40,16 +10,20 @@ def elim(formula):
         termA = elim(formula[:lastIffIndex]) # list up to end.
         termB = elim(formula[lastIffIndex + 1:])
         #formula = [[termA, '->', termB], '^', [termB, '->', termA]]
-        formula = [['!',termA, 'v', '!', termB], '^',['!', termB, 'v', termA]]
+        formula = [['!',termA, 'v', termB], '^',['!',termB, 'v', termA]]
     else: # eliminate implications
         impIndices = findall(formula, '->')
         if impIndices != []:
             lastImpIndex = impIndices[-1]
             termA = elim(formula[:lastImpIndex])
             termB = elim(formula[lastImpIndex + 1:])
-            formula =
+            formula = ['!', termA, 'v', termB]
     return formula
 
+def collapse_not(expr_list):
+    """
+    Takes a nested expression tree thing, checks for uncollapsed nots
+    """
 
 
 def findall(seq, elem):

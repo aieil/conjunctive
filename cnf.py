f1a1d793e298a863ed39f4b893d60469b9d5a39b
celph <celph@protonmail.ch>
2017-02-16 19:41:24 -0500

    started master function, added a fixed demorgan function

--- cnf.py
+++ cnf.py
@@ -16,11 +16,7 @@ def elim(formula):
             lastImpIndex = impIndices[-1]
             termA = elim(formula[:lastImpIndex])
             termB = elim(formula[lastImpIndex + 1:])
-<<<<<<< Updated upstream
             formula = ['!', termA, 'v', termB]
-=======
-            formula = []
->>>>>>> Stashed changes
     return formula
 
 # def collapse_not(expr_list):
@@ -32,19 +28,42 @@ def elim(formula):
 
 # def demorgan(formula): return neg([invert(symbol) if symbol in ('^', 'v') else neg(symbol) for symbol in formula])
 
+# this version is not correct and produces invalid output in some cases
+# do not use it. it should be deleted
 def demorgan(formula):
     output = []
     for symbol in formula:
-
         if symbol in ('^', 'v'):
-
             output.append(invert(symbol))
         else:
-
             output.append(neg(symbol))
 
     return neg(output)
 
+# The c is for correct because this version actually works
+# negates the expression and then performs the demorgan operation on its 
+# contents
+def demorgan_c(formula):
+    output = neg(formula)
+
+    if output[0] == '!':
+        demorgan_op(output[1])
+    else:
+        demorgan_op(output)
+
+    return output
+
+# demorgan operation: negates literals and bracketed expressions,
+# inverts conjunctions and disjunctions
+def demorgan_op(formula):
+    for symbol in enumerate(formula):
+        if symbol[1] in ('^', 'v'):
+            formula[symbol[0]] = invert(symbol[1])
+        # probably shouldn't ever happen?
+        elif symbol[1] == '!':
+            del formula[symbol[0]]
+        else:
+            formula[symbol[0]] = neg(symbol[1])
 
 def findall(seq, elem):
     """

f2ecc09ce296cb238369ba1b65492fd57cf2ab1a
Joseph Landy <14jnl@queensu.ca>
2017-03-02 19:30:14 -0500

    DONE FINALLY

--- cnf.py
+++ cnf.py
@@ -5,7 +5,41 @@ import sys
 import pprint
 
 
-def convert(formulaString):
+def convert(formula):
+    sys.setrecursionlimit(4000)
+    formula = copy.deepcopy(formula)
+
+    # step 1 remove <->, ->
+    formula = elim(formula)
+    formula = flatten_singletons_r(formula)
+    #print("after elimination:")
+    #pprint.pprint(formula)
+
+    # step 2 move negation inwards, eliminate double negation. #done.
+    formula = jlDemorgan_r(formula)
+    #print("after demorgans:")
+    #pprint.pprint(formula)
+
+    # step 3
+    #print("distributed formula")
+    formula = distribute_or(formula)
+    #pprint.pprint(formula)
+
+    flattened = list(iter_flatten(formula))
+    #print(flattened)
+    # now have to make this into sequences of cnf terms.
+    #andindices = findall(flattened, '^')
+
+    #print("clause form:")
+    cf =  splitlist(flattened, '^')
+    #print(cf)
+    return cf
+
+
+
+
+
+def convert_test(formulaString):
     sys.setrecursionlimit(3000) # yikes
 
     # using nestgen will not require any changes. It may mean some cases are
@@ -18,12 +52,24 @@ def convert(formulaString):
     print("after elimination:")
     pprint.pprint(formula)
 
-    # step 2 move negation inwards. #done.
+    # step 2 move negation inwards, eliminate double negation. #done.
     formula = jlDemorgan_r(formula)
     print("after demorgans:")
     pprint.pprint(formula)
 
     # step 3
+    print("distributed formula")
+    formula = distribute_or(formula)
+    pprint.pprint(formula)
+
+    flattened = list(iter_flatten(formula))
+    print(flattened)
+    # now have to make this into sequences of cnf terms.
+    #andindices = findall(flattened, '^')
+
+    print("clause form:")
+    cf =  splitlist(flattened, '^')
+    print(cf)
 
 
 
@@ -34,11 +80,8 @@ def elim(formula):
     if iffIndices != []: #eliminate top level iffs
         lastIffIndex = iffIndices[-1]
         termA = elim(formula[:lastIffIndex])     # start of list to <->
-        #flatten_singletons(termA)
         termB = elim(formula[lastIffIndex + 1:]) # <-> to end of list.
-        #flatten_singletons(termB)
-        #formula = [[termA, '->', termB], '^', [termB, '->', termA]]
-        #formula = [['!',termA, 'v', termB], '^',['!',termB, 'v', termA]]
+
         formula = [[neg(termA), 'v', termB], '^', [neg(termB), 'v', termA]]
     elif findall(formula, '->') != []:
         impIndices = findall(formula, '->')
@@ -46,11 +89,7 @@ def elim(formula):
             lastImpIndex = impIndices[-1]
             termA = elim(formula[:lastImpIndex])
             termB = elim(formula[lastImpIndex + 1:])
-            #formula = ['!', termA, 'v', termB]
-            # if type(termA) is str:
-            #     formula = [neg(termA), 'v', termB]
-            # else:
-            #     formula = [neg(termA), 'v', termB]
+
             formula = [neg(termA), 'v', termB]
     else:
         # otherwise, if there are no implications at the top level,
@@ -58,7 +97,6 @@ def elim(formula):
         for i in range(len(formula)):
             if type(formula[i]) is list:
                 formula[i] = elim(formula[i])
-    #flatten_singletons(formula)
 
     return formula
 
@@ -109,9 +147,6 @@ def distribute_or(formula):
     case 4-2 (A1 ^ A2) v (B1 v B2) ->
         (B1 v B2) v (A1 ^ A2) -> case 4-1
     case 4-3 (A1 v A2) v (B1 v B2) -> case 5
-
-
-
     """
     ######### case a #########
     if isAtom(formula):
@@ -122,20 +157,62 @@ def distribute_or(formula):
     op = formula[1]
     B = formula[2]
 
-    ## case 1
+    ## case 1 ## base case.
     if isAtom(A) and isAtom(B):
         return formula
 
-    ## case 3
-    if isAtom(B): # -- implies A is not an atom.
+    ## case 5 ##
+    if op == '^':
+        return [distribute_or(A), '^', distribute_or(B)]
+    # done case 5.
+
+    # implies operator
+
+    ## case 3 ##
+    if not isAtom(A) and isAtom(B):
         A, B = B, A # switch them around!
         # case 3 is now case 2
+        # can just reconstruct the formula and call the function recursively again.
+        # otherwise, probably better to just fall down to case 2.
+        # for the time being, former solution.
+        return distribute_or([A, 'v', B]) # will now go to case 2.
+
+    # a is atom but b is not:
+    if isAtom(A):
+        B1 = B[0]
+        Bop = B[1]
+        B2 = B[2]
+        if Bop == '^':
+            return [distribute_or([A, 'v', B1]), '^',
+                distribute_or([A, 'v', B2])]
+        else:
+            return [A, 'v', distribute_or(B)]
+
+    else:
+        return distrib_doublecase(A,B)
 
 
 
 
 
+def distrib_doublecase(A, B):
+    # A v B
+    # A v (B1 ^ B2)
+    B1 = B[0]
+    Bop = B[1]
+    B2 = B[2]
 
+    A1 = A[0]
+    Aop = A[1]
+    A2 = A[2]
+
+    if Bop == '^':
+        return [distribute_or([A,'v',B1]), '^', distribute_or([A, 'v', B2])]
+    elif Aop == '^': # and Bop == v
+        return [distribute_or([B, 'v', A1]), '^', distribute_or([B, 'v', A2])]
+    else: # Aop, Bop = v
+        return [[distribute_or(A1), 'v', distribute_or(A2)], '^',
+                        [distribute_or(B1), 'v', distribute_or(B2)]]
 
 
 
@@ -144,83 +221,6 @@ def isAtom(formula):
         return True
     else:
         return False
-
-
-
-#
-# def distribute_or(formula):
-#     formula = copy.deepcopy(formula)
-#     output = []
-#     for i in range(len(formula)):
-#         if type(formula[i]) is list: # see if this element is an atom or a subformula.
-#             output.append(distribute_or(formula[i]))
-#         elif formula[i] == 'v' and  type(formula[i+1]) is list:
-#             # remove the next item from the formula list, and call distribute
-#             # on it.
-#             # remove last element and set to previous symbol.
-#             prevSymbol = output.pop() # this will already be distributed.
-#             # lis.pop(i) I have no idea what this would be for.
-#             brackSymbol = distribute_or(formula.pop(i+1))
-#
-#             newExpression = []
-#             for j in range(len(brackSymbol)):
-#                 if brackSymbol[j] != '^': # note: there shouldn't be ors.
-#                     newExpression += [[prevSymbol, 'v', brackSymbol[j]], '^']
-#
-#
-#             newExpression.pop() # have to remove a trailing ^
-#         #else:
-#             # formula[i] is a string. Only thing is if it is a !.
-#             # demorgans will have been recursively applied beforehand, so it
-#             # won't happen.
-
-# def distribute_or2(formula):
-#
-#     # base case of the recursion: this is an atom. return it unchanged.
-#     if type(formula) is not list:
-#         return formula
-#     # else, it is a list representing a binary operator and 2 operands.
-#     # must have length 3.
-#     # it could still be a singleton list.
-#     # This entire thing will already be in precidence nested groups.
-#     formula = deepcopy(formula)
-#     output = []
-#
-#     if len(formula) == 1:
-#         # just check whether singleton lists will happen.
-#         print("found a singleton list!!")
-#         # if there are singleton lists, the solution is to both flatten
-#         # and call recursively.
-#         return distribute_or2(formula[0])
-#
-#
-#     # case 1: 2 elements joined by ^
-#     # in this case recur on each.
-#     if formula[1] == '^':
-#         A = formula[0]
-#         B = formula[2]
-#         output.append(distribute_or2(A))
-#         output.append('^')
-#         output.append(distribute_or2(B))
-#         return output
-
-    # case 2: 2 elements joined by v
-    # transform, then call recusively on each new term.
-    #if formula[1] == 'v':
-
-    # I think that's how this would work. This is NOT very efficient. Gross.
-    # too much recursion.
-
-
-# def distribute_element(A, BandC):
-#     # take an element Av(B^C) and do the distribution operation.
-#     if len(BandC) == 1:
-#         # singleton list. flattens to [AvBandC[0]]
-#         # could be a singleton nested list [[aardvark]].
-#         # negation should be at the bottom level.
-#         return [A, 'v', BandC]
-#     elif len(BandC) == 3:
-#         return [[A, 'v', BandC[0]], '^', [A, 'v', BandC[2]]]
 
 
 
@@ -233,7 +233,7 @@ def flatten_singletons(lis):
 
 def flatten_singletons_r(lis):
     # recursively go through a list and flatten singletons.
-    # base case 1
+
     if type(lis) is not list:
         return lis
 
@@ -267,52 +267,8 @@ def iter_flatten(iterable):
             yield e
 
 
-# The c is for correct because this version actually works
-# negates the expression and then performs the demorgan operation on its
-# contents
-
-# def demorgan(formula): return neg([invert(symbol) if symbol in ('^', 'v') else neg(symbol) for symbol in formula])
-# def demorgan(formula):
-#     output = neg(formula)
-#     if output[0] == '!':
-#         demorgan_op(output[1])
-#     else:
-#         demorgan_op(output)
-#
-#     return output
-
-# demorgan operation: negates literals and bracketed expressions,
-# inverts conjunctions and disjunctions
-# def demorgan_op(formula):
-#     for symbol in enumerate(formula):
-#         if symbol[1] in ('^', 'v'):
-#             formula[symbol[0]] = invert(symbol[1])
-#         # probably shouldn't ever happen?
-#         elif symbol[1] == '!':
-#             del formula[symbol[0]]
-#         else:
-#             formula[symbol[0]] = neg(symbol[1])
-#     # JL added
-#     return formula
-#     # end JL added
-
-# applies demorgan's theorem recursively to resolve negated bracketed
-# expressions
-# def demorgan_r(formula):
-#     output = demorgan(formula)
-#     #print(output)
-#     i = 0
-#     while i < len(output):
-#         #print(output[i])
-#         if type(output[i]) == list and output[i][0] == '!':
-#             output[i] = demorgan_r(output[i])
-#
-#         i += 1
-#
-#     return output
 
 def jlDemorgan_r(formula):
-    print("length of formula = ", len(formula))
     # output will be unchanged if length is not 2
     output = jlDemorgan(formula)
     # output should now be of the form AvB or else simply an atom
@@ -355,6 +311,25 @@ def collapse_double_negation(formula):
         return formula[1][1]
     else:
         return formula
+
+
+def splitlist(jlist, elem):
+    if elem not in jlist:
+        return [jlist] # return the list wrapped in another list.
+    split = []
+    i = 0
+    sublist = []
+    while i < len(jlist):
+        if not jlist[i] == elem:
+            sublist.append(jlist[i])
+        else:
+            split.append(sublist)
+            sublist = []
+        i += 1
+    split.append(sublist)
+
+    return split
+
 
 
 def findall(seq, elem):

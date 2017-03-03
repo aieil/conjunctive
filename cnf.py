570992c414f4989e74d71c9206929c6443be2226
Joseph Landy <14jnl@queensu.ca>
2017-03-02 19:40:30 -0500

    removed some prints.

--- cnf.py
+++ cnf.py
@@ -49,27 +49,29 @@ def convert_test(formulaString):
     # step 1 remove <->, ->
     formula = elim(formula)
     formula = flatten_singletons_r(formula)
-    print("after elimination:")
+    #print("after elimination:")
     pprint.pprint(formula)
 
     # step 2 move negation inwards, eliminate double negation. #done.
     formula = jlDemorgan_r(formula)
-    print("after demorgans:")
+    #print("after demorgans:")
     pprint.pprint(formula)
 
     # step 3
-    print("distributed formula")
+    #print("distributed formula")
     formula = distribute_or(formula)
     pprint.pprint(formula)
 
     flattened = list(iter_flatten(formula))
-    print(flattened)
+    #print(flattened)
     # now have to make this into sequences of cnf terms.
     #andindices = findall(flattened, '^')
 
-    print("clause form:")
+    #print("clause form:")
     cf =  splitlist(flattened, '^')
-    print(cf)
+    #print(cf)
+
+    return cf
 
 
 
